3a53328b274789d811de2e78f0ffbb2a4f247571
KJ <grazerjink@gmail.com>
2017-11-08 15:48:35 +0700

    Completed base source codes

--- BanLaptopC2C-ejb/build/generated-sources/ap-source-output/ejb/entities/NguoiMua_.java
+++ BanLaptopC2C-ejb/build/generated-sources/ap-source-output/ejb/entities/NguoiMua_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-08T15:31:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-08T15:47:05")
 @StaticMetamodel(NguoiMua.class)
 public class NguoiMua_ { 
 
--- BanLaptopC2C-ejb/build/generated-sources/ap-source-output/ejb/entities/TinhTrang_.java
+++ BanLaptopC2C-ejb/build/generated-sources/ap-source-output/ejb/entities/TinhTrang_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-08T15:31:55")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-11-08T15:47:05")
 @StaticMetamodel(TinhTrang.class)
 public class TinhTrang_ { 
 
