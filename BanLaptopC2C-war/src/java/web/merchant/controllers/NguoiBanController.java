067e468d4af151db12498f6e56f352d5285b8f57
KJ <grazerjink@gmail.com>
2017-12-12 23:23:43 +0700

    merge source Vi

--- BanLaptopC2C-war/src/java/web/merchant/controllers/NguoiBanController.java
+++ BanLaptopC2C-war/src/java/web/merchant/controllers/NguoiBanController.java
@@ -5,7 +5,6 @@
  */
 package web.merchant.controllers;
 
-import ejb.entities.DanhGia;
 import ejb.entities.NguoiBan;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import web.services.DanhGiaService;
 import web.services.NguoiBanService;
 
 /**
